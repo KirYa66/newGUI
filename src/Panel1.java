133716356a7b14cfae730aed9d0b323be6f00441
kirill <kir.yakimov@mail.ru>
2021-05-17 17:35:52 +0300

    17/08/2021 version 2

--- src/Panel1.java
+++ src/Panel1.java
@@ -9,10 +9,20 @@ public class Panel1 {
 
     private void go() {
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         panel.setBackground(Color.darkGray);
+
+        JButton button = new JButton("shock me");
+        panel.add(button);
+        JButton buttonTwo = new JButton("bliss");
+        panel.add(buttonTwo);
+        JButton buttonThree = new JButton("huh?");
+        panel.add(buttonThree);
+        //frame.getContentPane().add(BorderLayout.EAST,panel);
+
         frame.getContentPane().add(BorderLayout.EAST,panel);
-        frame.setSize(200,200);
+        frame.setSize(250,200);
         frame.setVisible(true);
     }
 }
